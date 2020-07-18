5d44db372951ac1a7071d3cd0f927440cbe6f2b7
Silas Mahler <silas-mahler@t-online.de>
2020-07-18 16:24:37 +0200

    .

---
+++ src/main/java/de/compilerbau/NewAwkCompiler/NewAwkCompilerApplication.java
@@ -0,0 +1,13 @@
+package de.compilerbau.NewAwkCompiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewAwkCompilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewAwkCompilerApplication.class, args);
+	}
+
+}
