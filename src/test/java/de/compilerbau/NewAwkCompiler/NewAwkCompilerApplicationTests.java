551181784678ef8730bf772b411d58516b760ed2
Silas Mahler <silas-mahler@t-online.de>
2020-07-27 13:36:59 +0200

    javacc21 lexing and parsing working

--- src/test/java/de/compilerbau/NewAwkCompiler/NewAwkCompilerApplicationTests.java
+++ src/test/java/de/compilerbau/NewAwkCompiler/NewAwkCompilerApplicationTests.java
@@ -1,28 +1,13 @@
 package de.compilerbau.NewAwkCompiler;
 
-import de.compilerbau.NewAwkCompiler.generated.NewAwkParser;
-import de.compilerbau.NewAwkCompiler.generated.Token;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.FileNotFoundException;
-import java.io.StringReader;
-import java.util.List;
 
 @SpringBootTest
 class NewAwkCompilerApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	public static List<Token> tokenize(String raw) {
-		try {
-			return Tokens.tokenize(new NewAwkParser(new StringReader(raw)));
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 }
